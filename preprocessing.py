818cf05417bffef075f79b6cf7f9918d888e48d2
Girin Chutia <71626493+GirinChutia@users.noreply.github.com>
2023-02-19 19:49:42 +0530

    add image bytes

---
+++ preprocessing.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import io
+
+def get_image_from_bytes(binary_image, max_size=1024):
+    input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
+    width, height = input_image.size
+    resize_factor = min(max_size / width, max_size / height)
+    resized_image = input_image.resize((int(input_image.width * resize_factor),
+                                        int(input_image.height * resize_factor)))
+    return resized_image
+
+def image_to_byte_array(image: Image) -> bytes:
+    imgByteArr = io.BytesIO()
+    image.save(imgByteArr, format=image.format)
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
